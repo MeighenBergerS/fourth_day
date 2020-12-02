Corrected Error in the FoV calculation

fourth_day/config.py
@@ -91,6 +91,7 @@ _baseconfig = {
             "y_pos": 10.,
         },
         # Detector positions
+        # Note that 0. deg is defined as parallel to the x-axis
         "detector properties": {
             # Point-like detector
             "point": {
@@ -99,6 +100,7 @@ _baseconfig = {
                 "det num": 1,
                 "x_offsets": [0.],
                 "y_offsets": [0.],
+                "angle offset": 90.,  # In which direction the detector points
                 "opening angle": 60.,
                 "wavelength acceptance": [
                     [0., 1000.]
@@ -115,7 +117,8 @@ _baseconfig = {
                 "y_offsets": np.array(
                     [0.,0.1,0.,-0.1,  0.12,0.12,-0.12,-0.12, 0.04,0.2,-0.04,-0.2]
                 ) / 2.,
-                "opening angle": 25., # from dark box rotation test result: +-25 degrees
+                "angle offset": 90.,  # In which direction the detector points
+                "opening angle": 25.,  # 25., # from dark box rotation test result: +-25 degrees
                 "wavelength acceptance": [ #position number,center wavelength,quantum efficiency
                     [395., 405.,0.26], #0,400
                     [505., 515.,0.16], #1,510
@@ -212,7 +215,7 @@ _baseconfig = {
         'pdf max light': 'Gamma',
         # TODO: Make this a distribution using data
         # Emission duration
-        'emission duration': 120.,
+        'emission duration': 100.,
         # The probability distributions used for the pulse shapes
         # Currently supported:
         #   - 'Gamma':
@@ -220,7 +223,7 @@ _baseconfig = {
         #   - 'Normal':
         #       A gaussian distribution
         'pdf pulse': {
-            'pdf': 'Gamma',
+            'pdf': 'LogNorm',
             'mean': [5., 10.],
             'sd': [3., 5.]
         },

fourth_day/lucifer.py
@@ -72,6 +72,17 @@ class Lucifer(object):
                     config["scenario"]["detector"]["type"]
                     ]
             )
+            # The acceptance region
+            self._acceptance_angles = [
+                self._det_geom["angle offset"] -
+                self._det_geom["opening angle"] / 2.,
+                self._det_geom["angle offset"] +
+                self._det_geom["opening angle"] / 2.
+            ]
+            _log.debug(
+                "The acceptance angles are: %.1f and %.1f" %(
+                    self._acceptance_angles[0], self._acceptance_angles[1])
+            )
         # Catching some errors
         except:
             raise KeyError(
@@ -137,11 +148,9 @@ class Lucifer(object):
         ])
         # To degrees
         angles = np.degrees(angles)
-        # Shifting the 0 line of the angles
-        angles = angles + (90. - self._det_geom["opening angle"])
-        angles = np.mod(angles, 360.)
-        # Checking with opening angles
-        angles[angles > self._det_geom["opening angle"]] = 0.
+        # Checking if within opening angles
+        angles[angles < self._acceptance_angles[0]] = 0.
+        angles[angles > self._acceptance_angles[1]] = 0.
         # Converting to 1 and zeros
         bool_arr = angles.astype(bool)
         # Acceptance arr
@@ -221,4 +230,11 @@ class Lucifer(object):
         _log.debug("Finished the attenuation calculation")
         end = time()
         _log.info("Propagation simulation took %f seconds" % (end - start))
+        # Checking if any light reached the detector and warning the user if
+        # not
+        anything_reached = not np.any(arriving)
+        if anything_reached:
+            _log.warning("No light has reached the detector! " +
+                         "This is usually due to the opening angle of the "+
+                         "detector.")
         return arriving